9aa045d7315246a5fa7ac8d83f0958c7699de03d
Shubham <sm.phoujdar@gmail.com>
2021-03-16 09:35:11 +0530

    Added : Player selects symbol and program assigns remaining symbol to computer.

--- src/main/java/newTicTacToe/newTicTacToe.java
+++ src/main/java/newTicTacToe/newTicTacToe.java
@@ -1,19 +1,25 @@
 package newTicTacToe;
 
+import java.util.Scanner;
+
 public class newTicTacToe {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 		
-		//UC1
 		char[] board = createBoard();
-				
 		
+		char playerSymbol = playerChooseSymbol(input);
+		char computerSymbol = '-';
+		if (playerSymbol == 'X' || playerSymbol == 'x' ) {
+			computerSymbol = 'O';
+		}else { computerSymbol = 'X'; }
 		
 	}
 
 	
-	
+	//UC1
 	private static char[] createBoard() {
 		char[] board = new char[10];
 		for(int i =0; i < board.length; i++) {
@@ -21,6 +27,29 @@ public class newTicTacToe {
 		}
 		return board;
 	}
+	
+	
+	//UC2
+	public static char playerChooseSymbol(Scanner sc) {
+		
+		System.out.println("Please Select your symbol [X] or [O]");
+		
+		while(true) {	
+			
+			char playerChar = sc.next().charAt(0);
+			
+			if (Character.compare(playerChar, 'x') == 0 || Character.compare(playerChar, 'X') == 0) {
+				System.out.println("Player selected character [X].");
+				return 'X';
+			}
+			else if (Character.compare(playerChar, 'o') == 0 || Character.compare(playerChar, 'O') == 0) {
+				System.out.println("Player selected character [O].");
+				return 'O';
+			}
+			else {System.out.println("Enter only enter the alphabets X or O.");}
+		}
+			
+	}	
 	
 }
 
