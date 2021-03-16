80ac5171f15ca79e0a63e0ef00c627faf30dfa98
Shubham <sm.phoujdar@gmail.com>
2021-03-16 08:56:23 +0530

    Added : Created board and intialised it to '-' using static function.

--- src/main/java/newTicTacToe/newTicTacToe.java
+++ src/main/java/newTicTacToe/newTicTacToe.java
@@ -3,7 +3,25 @@ package newTicTacToe;
 public class newTicTacToe {
 
 	public static void main(String[] args) {
-		System.out.printf("Welcome");
+		
+		
+		//UC1
+		char[] board = createBoard();
+				
+		
+		
+	}
+
+	
+	
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i =0; i < board.length; i++) {
+			board[i] = '-';
+		}
+		return board;
 	}
 	
 }
+
+
