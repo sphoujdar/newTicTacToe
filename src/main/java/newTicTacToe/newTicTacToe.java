e7a1d4b6fae0f408567a515cc48e6a58d08b3a58
Shubham <sm.phoujdar@gmail.com>
2021-03-18 09:16:05 +0530

    Added : Computer can play to win if a winning move available in its current move.

--- src/main/java/newTicTacToe/newTicTacToe.java
+++ src/main/java/newTicTacToe/newTicTacToe.java
@@ -1,10 +1,3 @@
-/*Query 1 - Recursive call to function from the function , What exactly happens ?
- * 			eg. Wrong toss input.*/
-
-/*Query 2 - Using debugger instead of printing to console.*/
-
-
-
 package newTicTacToe;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -18,18 +11,26 @@ public class newTicTacToe {
 		Scanner input = new Scanner(System.in);
 		char[] board = createBoard();
 		char playerSymbol = playerChooseSymbol(input), opponentSymbol = (playerSymbol == 'X') ? 'O' : 'X';
+		int gameTypeChoice = getGameTypeChoice(input);
 		char isPlayerTurnNow = toss(input);
 
 
 		do {
-
-			if (isPlayerTurnNow == 'W') {
+			if (isPlayerTurnNow == 'P') {
 				playerMove(board, input, playerSymbol);
-				isPlayerTurnNow = 'L';
-			} else if (isPlayerTurnNow == 'L'){
-				playerMove(board, input, opponentSymbol);
-				isPlayerTurnNow = 'W';
-			} else {
+				isPlayerTurnNow = 'O';
+			}
+			else if (isPlayerTurnNow == 'O') {
+				if (gameTypeChoice == 2) {
+					playerMove(board, input, opponentSymbol);
+					isPlayerTurnNow = 'P';
+				}
+				else if (gameTypeChoice == 1) {
+					computerPlays(board, opponentSymbol,playerSymbol);
+					isPlayerTurnNow = 'P';
+				}
+			}
+			else {
 				isPlayerTurnNow = toss(input);
 			}
 
@@ -143,8 +144,8 @@ public class newTicTacToe {
 			System.out.println("A coin was tossed");
 		}
 
-		char playerWonOrNot = ((((int) (Math.random() * 10)) % 2) == 0) ? 'W': 'L';
-		if (playerWonOrNot == 'W')
+		char playerWonOrNot = ((((int) (Math.random() * 10)) % 2) == 0) ? 'P': 'O';
+		if (playerWonOrNot == 'P')
 			System.out.printf("Coin turned up [%s]. You Won. You will play first.\n", playerTossCall);
 		else
 			System.out.printf("Coin turned up [%s]. You lost. Your opponent will play first.\n",
@@ -203,14 +204,147 @@ public class newTicTacToe {
 			}
 		}
 
-		String doesBoardHaveEmptySpace = "" + board[0] + board[1] + board[2] + board[3] + board[4]
-							  + board[5] + board[6] + board[7] + board[8];
+		String doesBoardHaveEmptySpace = "" +  board[1] + board[2] + board[3] + board[4]
+							  + board[5] + board[6] + board[7] + board[8] + board[9];
 		if (doesBoardHaveEmptySpace.contains("-")){
 			return 'E';
 		}
 
 		return 'T';
 	}
+
+	//UC8
+	public static boolean threeCharCompare(char first , char second, char third){
+		return first == second && second == third;
+	}
+
+	//UC8
+	public  static int getGameTypeChoice(Scanner sc){
+
+		int gameTypeChoiceInput;
+
+		do{
+			System.out.println("Please input choice\n" +
+					"[1] Play against Computer\n" +
+					"[2] 2 Player game\n");
+			gameTypeChoiceInput = sc.nextInt();
+		} while(gameTypeChoiceInput != 1 && gameTypeChoiceInput != 2);
+
+
+		return gameTypeChoiceInput;
+
+	}
+
+	//UC8
+	public static void computerPlays(char[] board , char computerCharacter, char playerCharacter){
+
+		int indexChosen = computerChoosesIndex(board,computerCharacter, playerCharacter);
+		board[indexChosen] = computerCharacter;
+		System.out.printf("Computer Played at position [%d]\n",indexChosen);
+	}
+
+
+	//UC8
+	public static int computerChoosesIndex(char[] board , char computerCharacter, char playerCharacter){
+
+		//playToWin Logic
+		for (int caseNum = 1; caseNum < 10; caseNum++) {
+			switch (caseNum) {
+				case 9:
+					if (board[9] != playerCharacter && (
+						threeCharCompare(board[3] , board[6] ,computerCharacter) ||
+						threeCharCompare(board[7] , board[8] ,computerCharacter) ||
+						threeCharCompare(board[1] , board[5] ,computerCharacter)))
+					{
+						return 9;
+					}
+					break;
+				case 8:
+					if (board[8] != playerCharacter && (
+						threeCharCompare(board[7] , board[9] ,computerCharacter) ||
+						threeCharCompare(board[2] , board[5] ,computerCharacter)))
+					{
+						return 8;
+					}
+					break;
+				case 7:
+					if (board[7] != playerCharacter && (
+						threeCharCompare(board[1] , board[4] ,computerCharacter) ||
+						threeCharCompare(board[9] , board[8] ,computerCharacter) ||
+						threeCharCompare(board[3] , board[5] ,computerCharacter)))
+					{
+						return 7;
+					}
+					break;
+				case 6:
+					if (board[6] != playerCharacter && (
+						threeCharCompare(board[3] , board[9] ,computerCharacter) ||
+						threeCharCompare(board[4] , board[5] ,computerCharacter)))
+					{
+						return 6;
+					}
+					break;
+				case 5:
+					if (board[5] != playerCharacter && (
+						threeCharCompare(board[2] , board[8] ,computerCharacter) ||
+						threeCharCompare(board[4] , board[6] ,computerCharacter) ||
+						threeCharCompare(board[1] , board[9] ,computerCharacter) ||
+						threeCharCompare(board[3] , board[7] ,computerCharacter)))
+					{
+						return 5;
+					}
+					break;
+				case 4:
+					if (board[4] != playerCharacter && (
+						threeCharCompare(board[5] , board[6] ,computerCharacter) ||
+						threeCharCompare(board[1] , board[7] ,computerCharacter)))
+					{
+						return 4;
+					}
+					break;
+				case 3:
+					if (board[3] != playerCharacter && (
+						threeCharCompare(board[9] , board[6] ,computerCharacter) ||
+						threeCharCompare(board[7] , board[5] ,computerCharacter) ||
+						threeCharCompare(board[1] , board[2] ,computerCharacter)))
+					{
+						return 3;
+					}
+					break;
+				case 2:
+					if (board[2] != playerCharacter && (
+						threeCharCompare(board[5] , board[8] ,computerCharacter) ||
+						threeCharCompare(board[1] , board[3] ,computerCharacter)))
+					{
+						return 2;
+					}
+					break;
+				case 1:
+					if (board[1] != playerCharacter && (
+						threeCharCompare(board[3] , board[2] ,computerCharacter) ||
+						threeCharCompare(board[7] , board[4] ,computerCharacter) ||
+						threeCharCompare(board[9] , board[5] ,computerCharacter)))
+					{
+						return 1;
+					}
+					break;
+			}
+		}
+
+		/* Above code says play to win.
+		* Below code says if no win move,
+		* then play at first open spot.
+		* This is the logic for UC8.*/
+
+		for (int i = 1; i < 10; i++) {
+			if(board[i] == '-'){
+				return i;
+			}
+
+		}
+		return 11;
+	}
+
 
 }
 
