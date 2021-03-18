f338d4b6215a768b878d3cb58c8c408952a91b41
Shubham <sm.phoujdar@gmail.com>
2021-03-18 07:00:13 +0530

    Added : Game can check who won after every move.

--- src/main/java/newTicTacToe/newTicTacToe.java
+++ src/main/java/newTicTacToe/newTicTacToe.java
@@ -6,168 +6,211 @@
 
 
 package newTicTacToe;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class newTicTacToe {
 
 	public static void main(String[] args) {
 
-		
-		
+
 		//Various Variables used in main()
 		Scanner input = new Scanner(System.in);
 		char[] board = createBoard();
-		char playerSymbol = playerChooseSymbol(input) , opponentSymbol = (playerSymbol == 'X') ? 'O' : 'X' ;
-		boolean isPlayerTurnNow = toss(input) ;
-		
-		showBoard(board);
+		char playerSymbol = playerChooseSymbol(input), opponentSymbol = (playerSymbol == 'X') ? 'O' : 'X';
+		char isPlayerTurnNow = toss(input);
+
+
 		do {
-			if(!isPlayerTurnNow) 
-			{
-				opponentPlays(board);
-				isPlayerTurnNow = true;
-			}
-			else
-			{
+
+			if (isPlayerTurnNow == 'W') {
 				playerMove(board, input, playerSymbol);
-				isPlayerTurnNow = false;
+				isPlayerTurnNow = 'L';
+			} else if (isPlayerTurnNow == 'L'){
+				playerMove(board, input, opponentSymbol);
+				isPlayerTurnNow = 'W';
+			} else {
+				isPlayerTurnNow = toss(input);
 			}
-			
-		} while (true);
-		
+
+			switch (whoWon(board)){
+				case 'T' :
+					System.out.println("Board has no empty spaces , Game ended in a tie.");
+					break;
+				case 'X' :
+					showBoard(board);
+					System.out.println("Player [X] won the game.");
+					break;
+				case 'O' :
+					showBoard(board);
+					System.out.println("Player [O] won the game.");
+					break;
+			}
+
+		} while (whoWon(board) == 'E');
+
 	}
 
-	
+
 	//UC1
 	private static char[] createBoard() {
 		char[] board = new char[10];
-		for(int i =0; i < board.length; i++) {
-			board[i] = '-';
-		}
+		Arrays.fill(board,'-');
 		return board;
 	}
-	
-	
+
+
 	//UC2
 	public static char playerChooseSymbol(Scanner sc) {
-		
+
 		System.out.println("Please Select your symbol [X] or [O]");
-		
-		while(true) {	
-			
+
+		while (true) {
+
 			char playerChar = sc.next().charAt(0);
-			
-			if (Character.compare(playerChar, 'x') == 0 || Character.compare(playerChar, 'X') == 0) {
+
+			if (playerChar == 'x' || playerChar == 'X') {
 				System.out.println("You are Player [X] now.");
 				return 'X';
-			}
-			else if (Character.compare(playerChar, 'o') == 0 || Character.compare(playerChar, 'O') == 0) {
+			} else if (playerChar == 'o' || playerChar == 'O') {
 				System.out.println("You are Player [O] now");
 				return 'O';
+			} else {
+				System.out.println("Enter only enter the alphabets X or O.");
 			}
-			else {System.out.println("Enter only enter the alphabets X or O.");}
-		}	
+		}
 	}
-	
+
 	//UC3
 	public static void showBoard(char[] board1) {
-		
+
 		System.out.println("-------         Position Map-");
-		for (int i = 1 ; i< 10 ; i++) {
-			System.out.printf("|" + board1[i]);
-			if(i%3==0) {
-				System.out.printf("|         [%d][%d][%d]\n",i-2,i-1,i);
+		for (int i = 1; i < 10; i++) {
+			System.out.printf("|%s",board1[i]);
+			if (i % 3 == 0) {
+				System.out.printf("|         [%d][%d][%d]\n", i - 2, i - 1, i);
 			}
 		}
 		System.out.println("-------");
 	}
-	
-	
+
+
 	//UC4
 	public static void playerMove(char[] board2, Scanner Input, char playerChar) {
-		
-		System.out.printf("Player [%s] please make your move.",playerChar);
+
+		System.out.printf("Player [%s] please make your move.\n", playerChar);
 		int slotStatus = isIndexEmpty(board2, Input);
-		
-		if(slotStatus != 0) {
+
+		if (slotStatus != 0) {
 			board2[slotStatus] = playerChar;
-			showBoard(board2);
-		}
-		else {
+		} else {
 			System.out.println("\n\n !!! The position you selected is already filled !!! \n"
-					          + "!!! Please select empty position !!!\n\n");
+					+ "!!! Please select empty position !!!\n\n");
 			playerMove(board2, Input, playerChar);
 		}
 	}
-	
-	
+
+
 	//UC4 and UC5
 	public static int isIndexEmpty(char[] board1, Scanner input) {
-		
-			int index = 0;
-			
-		while(true) {
+
+		int index;
+		do{
+			showBoard(board1);
 			System.out.println("Please enter a position between 1 to 9.");
 			index = input.nextInt();
-			if (index > 0 && index <10 ) {
-				break;
-			}
-			
 		}
-		if ( board1[index] == 'X' || board1[index] == 'O') {
+		while(!(index > 0 && index < 10));
+
+		if (board1[index] == 'X' || board1[index] == 'O') {
 			return 0;
 		}
 		return index;
 	}
-	
-	
+
+
 	//UC6
-	public static boolean toss(Scanner sc) {
-		
+	public static char toss(Scanner sc) {
+
 		System.out.println("Type [H] or [T] for coin toss.");
 		char playerTossCall = sc.next().toUpperCase().charAt(0);
-				
-		if (playerTossCall != 'H' && playerTossCall != 'T') 
-			toss(sc);
-		else
-		{
+
+		if (playerTossCall != 'H' && playerTossCall != 'T') {
+			System.out.println("Please only call [H] or [T]");
+			return 'Z';
+		}
+		else {
 			System.out.println("A coin was tossed");
 		}
-		
-		boolean playerWonOrNot = ( (int) (Math.random()*10) % 2 == 0) ? true : false;
-		if (playerWonOrNot)
-			System.out.printf("Coin turned up [%s]. You Won. You will play first.\n",playerTossCall);
+
+		char playerWonOrNot = ((((int) (Math.random() * 10)) % 2) == 0) ? 'W': 'L';
+		if (playerWonOrNot == 'W')
+			System.out.printf("Coin turned up [%s]. You Won. You will play first.\n", playerTossCall);
 		else
 			System.out.printf("Coin turned up [%s]. You lost. Your opponent will play first.\n",
-							   (playerTossCall == 'H') ? 'T' : 'H' );
-		
-		return  playerWonOrNot;
-	}
-	
-	//UC7
-	public static void opponentPlays(char[] board) {
-		
-		System.out.println("No opponent move right now.");
-		showBoard(board);
-		
-	}
-	
-	//UC7
-	public static boolean winOrNot(char[] board , boolean isPlayerTurnNow, char lastPlayedSymbol) {
-		
-		if(board[0] == board[1] && board[1] == board[2] && board[2] == lastPlayedSymbol ||
-		   board[3] == board[4] && board[4] == board[5] && board[5] == lastPlayedSymbol ||
-		   board[6] == board[7] && board[7] == board[8] && board[8] == lastPlayedSymbol ||
-		   board[0] == board[4] && board[4] == board[8] && board[8] == lastPlayedSymbol ||
-		   board[2] == board[4] && board[4] == board[6] && board[6] == lastPlayedSymbol) 
-		{
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-}
+					(playerTossCall == 'H') ? 'T' : 'H');
 
+		return playerWonOrNot;
+	}
+
+
+	//UC7
+	public static char whoWon(char[] board) {
+
+
+		for (int caseNum = 0; caseNum < 8; caseNum++) {
+			switch (caseNum) {
+				case 0:
+					if (board[1] == board[2] && board[2] == board[3] && board[1] != '-'){
+						return board[1];
+					}
+					break;
+				case 1:
+					if (board[4] == board[5] && board[5] == board[6] && board[4] != '-'){
+						return board[4];
+					}
+						break;
+				case 2:
+					if (board[7] == board[8] && board[8] == board[9] && board[7] != '-'){
+						return board[7];
+					}
+					break;
+				case 3:
+					if (board[1] == board[4] && board[4] == board[7] && board[1] != '-'){
+						return board[1];
+					}
+					break;
+				case 4:
+					if (board[2] == board[5] && board[5] == board[8] && board[2] != '-'){
+						return board[2];
+					}
+					break;
+				case 5:
+					if (board[3] == board[6] && board[6] == board[9] && board[3] != '-'){
+						return board[3];
+					}
+					break;
+				case 6:
+					if (board[1] == board[5] && board[5] == board[9] && board[1] != '-'){
+						return board[1];
+					}
+					break;
+				case 7:
+					if (board[3] == board[5] && board[5] == board[7] && board[3] != '-'){
+						return board[3];
+					}
+					break;
+			}
+		}
+
+		String doesBoardHaveEmptySpace = "" + board[0] + board[1] + board[2] + board[3] + board[4]
+							  + board[5] + board[6] + board[7] + board[8];
+		if (doesBoardHaveEmptySpace.contains("-")){
+			return 'E';
+		}
+
+		return 'T';
+	}
+
+}
 
